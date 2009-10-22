07ddefaadc8df44837fbbab015e5ba6cfff3851b
Wes Rogers <wesrog@gmail.com>
2009-10-22 14:53:18 -0500

    changing verbiage

--- public/coversrc.js
+++ public/coversrc.js
@@ -1,5 +1,5 @@
 $(function(){
   $('input').bind('click',function(event){$(this).val('')});
-  $('#about').bind('click',function(event){$('#more-info').toggle('blind')});
+  $('#about').bind('click',function(event){$('#more-info').toggle('blind'); $('#about').blur();});
   $.get('/discogs/' + $('#artist')[0].innerHTML + '/' + $('#track')[0].innerHTML, function(data) { $('#discogs dl').html(data); });
 });
