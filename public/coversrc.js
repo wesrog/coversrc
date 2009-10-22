d9dfa42c9772609037dc27fff124a8c239bd8207
Wes Rogers <wesrog@gmail.com>
2009-10-22 11:51:04 -0500

    cleaning up js

--- public/coversrc.js
+++ public/coversrc.js
@@ -1,6 +1,5 @@
 $(function(){
   $('input').bind('click',function(event){$(this).val('')});
   $('#about').bind('click',function(event){$('#more-info').toggle('blind')});
-  //$('#discogs dl').load('discogs')
-  $.get('/discogs/' + $('#artist')[0].innerHTML + '/' + $('#track')[0].innerHTML, function(data) { $('#discogs dl').html(data); $('#loading').fadeOut(); });
+  $.get('/discogs/' + $('#artist')[0].innerHTML + '/' + $('#track')[0].innerHTML, function(data) { $('#discogs dl').html(data); });
 });
