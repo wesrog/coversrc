36608e95f0dbde4d7311e62f9bd6cff19377784d
Wes Rogers <wesrog@gmail.com>
2009-10-22 14:23:18 -0500

    added new logos

--- coversrc.rb
+++ coversrc.rb
@@ -32,8 +32,6 @@ get '/discogs/:artist/:track' do
     @search = DiscogsSearch.new(params[:artist], params[:track])
     etag Digest::MD5.hexdigest(params[:artist] + params[:track]) if production?
     haml :discogs, :layout => false
-  rescue OpenURI::HTTPError
-    haml :error, :layout => false
   end
 end
 
