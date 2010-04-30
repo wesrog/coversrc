9a1ed893c7713d7c5d8e238a0da3a8ba277a6e53
Wes Rogers <wesrog@gmail.com>
2010-04-29 20:36:14 -0700

    verbiage

--- spec/coversrc_spec.rb
+++ spec/coversrc_spec.rb
@@ -1,18 +1,18 @@
 require File.dirname(__FILE__) + '/spec_helper'
 
 describe "coversrc" do
-  it "should return a 200 on the homepage" do
+  it "will return a 200 on the homepage" do
     get '/'
     last_response.status.should == 200
     assert last_response.body.include?('music')
   end
 
-  it "should return a 200 on valid user" do
+  it "will return a 200 on valid user" do
     get '/wesr'
     last_response.status.should == 200
   end
 
-  it "should return a 404 on invalid user" do
+  it "will return a 404 on invalid user" do
     get '/wesra'
     get '/lastfm'
     last_response.status.should == 404
